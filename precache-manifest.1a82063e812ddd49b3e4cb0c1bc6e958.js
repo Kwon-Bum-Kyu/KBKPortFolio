6d6968cd8a5cba230c251c993698c085d9ed38b6
DEVKBK <bumkyu98@watchmile.com>
2021-04-05 17:00:48 +0900

    Updates

---
+++ precache-manifest.1a82063e812ddd49b3e4cb0c1bc6e958.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1308938a64537d78bce85ba279ef5911",
+    "url": "/KBKPortFolio/index.html"
+  },
+  {
+    "revision": "446581a7dbe4e44ea186",
+    "url": "/KBKPortFolio/static/css/main.e2c34c49.chunk.css"
+  },
+  {
+    "revision": "f0585afd58bab4c12498",
+    "url": "/KBKPortFolio/static/js/2.9a2180c5.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/KBKPortFolio/static/js/2.9a2180c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "446581a7dbe4e44ea186",
+    "url": "/KBKPortFolio/static/js/main.f52f302b.chunk.js"
+  },
+  {
+    "revision": "c6de43420ae546cbff35",
+    "url": "/KBKPortFolio/static/js/runtime-main.6a918af3.js"
+  },
+  {
+    "revision": "b88924d5d914bf9f3885f87c2ed76477",
+    "url": "/KBKPortFolio/static/media/sad-icon.b88924d5.png"
+  },
+  {
+    "revision": "b9d7d448a09d6aacb45d21aab2c6fd53",
+    "url": "/KBKPortFolio/static/media/your photo.b9d7d448.png"
+  }
+]);
